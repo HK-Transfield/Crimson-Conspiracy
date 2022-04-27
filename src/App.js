dc3cc4497c5342388ecd25ae4e926b684ee5223e
Transfield, Harmon Kurt <h.k.transfield1@gmail.com>
2022-04-28 11:19:50 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import GlitchedWriter, { wait } from 'glitched-writer';
+import './style.scss';
+
+
 
 function App() {
+
+
+  const Writer = new GlitchedWriter('#glitch_this', { letterize: true });
+
+  (async () => {
+    await wait(2500);
+    await Writer.write("THE VESSELS HAVE CONNECTED");
+
+    await wait(2500);
+    await Writer.write("WE WILL BEGIN THE NEXT PHASE");
+
+    await wait(2500);
+    await Writer.write("YOU ARE CLOSE");
+
+    await wait(2500);
+    await Writer.write("CLOSE TO THE NEXT KEY");
+
+    await wait(2500);
+    await Writer.write("IT IS NOT FAR");
+
+    await wait(2500);
+    await Writer.write("BUT BEWARE");
+
+    await wait(2500);
+    await Writer.write("THE EYE AND ITS DEMONS DRAW NEAR");
+
+    await wait(2500);
+    await Writer.write("THE FEAST IS UPON US");
+
+    await wait(2500);
+    await Writer.write("TIME IS SHORT");
+
+    await wait(2500);
+    await Writer.write("FAILURE WILL RESULT IN");
+
+    await wait(2500);
+    await Writer.write("ERADICATION");
+
+    await wait(2500);
+    await Writer.write("DO NOT FEAR");
+
+    await wait(2500);
+    await Writer.write("WE ARE WATCHING");
+
+    await wait(2500);
+    await Writer.write("WE ARE OBSERVING");
+
+    await wait(2500);
+    await Writer.write("WE SEE YOUR PERFORMANCE");
+
+    await wait(2500);
+    await Writer.write("I WILL VISIT YOU");
+
+    await wait(2500);
+    await Writer.write("VERY");
+
+    await wait(2500);
+    await Writer.write("SOON");
+
+    await wait(2500);
+    await Writer.write(" CRIMSON CONSPIRACY 29.04.2022");
+  })();
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="text" id="glitch_this"></div>
     </div>
   );
 }
