bab22c8b1be8088d3aba2c52e5b071a2d364e22c
Transfield, Harmon Kurt <h.k.transfield1@gmail.com>
2022-04-29 17:33:21 +1200

    fix soon

--- src/App.js
+++ src/App.js
@@ -1,47 +1,41 @@
 //import './App.css';
-import './style.scss';
-import GlitchedWriter from 'glitched-writer';
-import { useEffect } from 'react';
-
-
-const phrases = [
-  "THE VESSEL HAS CONNECTED",
-  "WE WILL BEGIN THE NEXT PHASE",
-  "YOU ARE CLOSE",
-  "CLOSE TO THE NEXT KEY",
-  "IT IS NOT FAR",
-  "BUT BEWARE",
-  "THE EYE AND ITS DEMONS DRAW NEAR",
-  "THE FEAST IS UPON US",
-  "TIME IS SHORT",
-  "FAILURE WILL RESULT IN",
-  "ERADICATION",
-  "DO NOT FEAR",
-  "WE ARE WATCHING",
-  "WE ARE OBSERVING",
-  "WE SEE YOUR PERFORMANCE",
-  "I WILL VISIT YOU",
-  "VERY",
-  "SOON",
-  "CRIMSON CONSPIRACY 29.04.2022",
-]
+import { Route, Link, Routes } from 'react-router-dom';
+import Hallway from './routes/Hallway';
+import Painting from './routes/Painting';
+import Window from './routes/Window';
+import Message from './routes/Message';
 
 
 function App() {
 
-  useEffect(() => {
-    const glitch = document.getElementById('glitch_this');
-    const Writer = new GlitchedWriter(glitch, { letterize: true });
-    Writer.queueWrite(phrases, 2500, false)
-  })
+
 
   return (
     <div className="App">
-      <div className="text" id="glitch_this">
-        <div id="phrases" style={{ display: 'none' }}>
-
-        </div>
+      <div>
+        <nav>
+          <ul id="navigation">
+            <li>
+              <Link to="/hallway">Home</Link>
+            </li>
+            <li>
+              <Link to="/findme">About</Link>
+            </li>
+            <li>
+              <Link to="/look">Contact</Link>
+            </li>
+            <li>
+              <Link to="/message">Contact</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
+      <Routes>
+        <Route path="/hallway"><Hallway /></Route>
+        <Route path="/findme"><Painting /></Route>
+        <Route path="/look"><Window /></Route>
+        <Route path="/message"><Message /></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/routes/Message.js
@@ -0,0 +1,49 @@
+//import './App.css';
+import '../styles/style.scss';
+import GlitchedWriter from 'glitched-writer';
+import { useEffect } from 'react';
+
+
+const phrases = [
+    "THE VESSEL HAS CONNECTED",
+    "WE WILL BEGIN THE NEXT PHASE",
+    "YOU ARE CLOSE",
+    "CLOSE TO THE NEXT KEY",
+    "IT IS NOT FAR",
+    "BUT BEWARE",
+    "THE EYE AND ITS DEMONS DRAW NEAR",
+    "THE FEAST IS UPON US",
+    "TIME IS SHORT",
+    "FAILURE WILL RESULT IN",
+    "ERADICATION",
+    "DO NOT FEAR",
+    "WE ARE WATCHING",
+    "WE ARE OBSERVING",
+    "WE SEE YOUR PERFORMANCE",
+    "I WILL VISIT YOU",
+    "VERY",
+    "SOON",
+    "CRIMSON CONSPIRACY 29.04.2022",
+]
+
+
+function Message() {
+
+    useEffect(() => {
+        const glitch = document.getElementById('glitch_this');
+        const Writer = new GlitchedWriter(glitch, { letterize: true });
+        Writer.queueWrite(phrases, 2500, false)
+    })
+
+    return (
+        <div className="App">
+            <div className="text" id="glitch_this">
+                <div id="phrases" style={{ display: 'none' }}>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Message;
